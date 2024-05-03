5b580556cc1922e34e8db13ddfb2c99901b2082f
Adrian Utomo <adrianhakimutomo@gmail.com>
2024-05-03 20:07:03 +0700

    chore: add CORS

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -49,9 +49,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'chatbot.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
