3ea18ec86218fff2dacfd8cd21da553b63f525e9
Adrian Utomo <adrianhakimutomo@gmail.com>
2024-05-14 20:30:52 +0700

    chore: change function to preprocess_question

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .ai_model import preprocess_text, generate_text_sampling_top_p_nucleus_22
+from .ai_model import preprocess_question, generate_text_sampling_top_p_nucleus_22
 
 @api_view(['GET'])
 def helloWorld(request):
@@ -10,7 +10,7 @@ def helloWorld(request):
 def getAnswer(request):
     question = request.data.get('question')
     if question:
-        question = preprocess_text(question)
+        question = preprocess_question(question)
         answer = generate_text_sampling_top_p_nucleus_22(question)
         return Response({"question": question, "answer": answer})
     else:
